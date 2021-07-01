72a464101f0bcff6e14c9b923c3a54f06442146a
sutanu5 <sutanu.sain@gmail.com>
2021-07-01 18:48:19 +0530

    adding build file

---
+++ build/precache-manifest.d5a4373298d1b97d69d2663422ef789a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575a8c651dd85f40fb2dc47c639f36a1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "525bf0ade603d13691fc",
+    "url": "/calculator/static/css/main.7277392b.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/calculator/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "525bf0ade603d13691fc",
+    "url": "/calculator/static/js/main.d6a8e887.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
